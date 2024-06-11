224c9fdcf1744b1a619442a1faa60cd0b200d4f9
Kidda0Snoogans <mx.astridburris@gmail.com>
2024-06-11 10:59:13 -0400

    esrdftyghuij

--- fairyTale.js
+++ fairyTale.js
@@ -1,26 +1,50 @@
 // 1. Create a variable named `place` and set the value to a string that says whatever place you want.
 
+let Place = "Club";
+
 // 2. Create a variable named `creatureName` and set the value to a string that says whatever name you want to give the creature.
+
+let Creature_Name = "Hooker";
 
 // 3. Create a variable named `color` and set the value to a string that says whatever color you want.
 
+let Color = "Black";
+
 // 4. Create a variable named `color2` and set the value to a string that says a different color you want.
+
+let Color_2 = "Purple";
 
 // 5. Create a variable named `emotion` and set the value to a string that says whatever emotion you want.
 
+let  Emotion = "Pissed";
+
 // 6. Create a variable named `character` and set the value to a string that says whatever name for a character you want.
+
+let Chr = "ScrotumBalls";
 
 // 7. Create a variable named `number` and set the value to a number greater than 1.
 
+let Number = "69";
+
 // 8. Create a variable named `item` and set the value to a string that says whatever item/thing you want.
+
+let Item = "Big white horse";
 
 // 9. Create a variable named `item2` and set the value to a string that says a different item/thing you want.
 
+let Item_2 = "Big ass sword";
+
 // 10. Create a variable named `verb` and set the value to a string that says whatever verb you want.
+
+let Verb = "Burned";
 
 // 11. Create a variable named `adverb` and set the value to a string that says whatever adverb you want.
 
+let Adverb = "Quickly";
+
 // 12. Create a variable named `number2` and set the value to a number greater than 1.
+
+let Number_2 = "273";
 
 
 
@@ -31,19 +55,19 @@
 // - Afterwards, run your code and press the button to see your story ✨
 
 document.querySelector("button").addEventListener("click", function() {
-    document.querySelector(".place").innerHTML = null;
-    document.querySelector(".creature-name").innerHTML = null;
-    document.querySelector(".color").innerHTML = null;
-    document.querySelector(".color-2").innerHTML = null;
-    document.querySelector(".emotion").innerHTML = null;
-    document.querySelector(".character").innerHTML = null;
-    document.querySelector(".creature-name-again").innerHTML = null;
-    document.querySelector(".number").innerHTML = null;
-    document.querySelector(".item").innerHTML = null;
-    document.querySelector(".character-again").innerHTML = null;
-    document.querySelector(".item-2").innerHTML = null;
-    document.querySelector(".verb").innerHTML = null;
-    document.querySelector(".adverb").innerHTML = null;
-    document.querySelector(".number-2").innerHTML = null;
+    document.querySelector(".Place").innerHTML = null;
+    document.querySelector(".Creature_Name").innerHTML = null;
+    document.querySelector(".Color").innerHTML = null;
+    document.querySelector(".Color_2").innerHTML = null;
+    document.querySelector(".Emotion").innerHTML = null;
+    document.querySelector(".Chr").innerHTML = null;
+    document.querySelector(".Creature_Name-again").innerHTML = null;
+    document.querySelector(".Number").innerHTML = null;
+    document.querySelector(".Item").innerHTML = null;
+    document.querySelector(".Chr-again").innerHTML = null;
+    document.querySelector(".Item_2").innerHTML = null;
+    document.querySelector(".Verb").innerHTML = null;
+    document.querySelector(".Adverb").innerHTML = null;
+    document.querySelector(".Number_2").innerHTML = null;
     document.querySelector(".story-container").style = "display:block";
-});
+})
--- variables.js
+++ variables.js
@@ -1,13 +1,18 @@
 // CODE ALONG, PART 1
-let favoriteColor;
+
+let Favorite_Color;
+Favorite_Color = "Sky Blue"
 
 // 1. Create a variable called `favoriteFood`.
 
+let Favorite_Food;
+Favorite_Food = "Strippers"
 
 // 2. Create a variable called `favoriteNumber`.
 
-
-
+let Favorite_Number = 273;
+// Dad how many women have you slept with? 5? 6?
+// 273!!
 
 // CODE ALONG, PART 2
 //  3. Assign values for each variables.
